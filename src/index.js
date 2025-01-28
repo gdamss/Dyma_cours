c12de32c32bbaf6aec10feb9becd0c41ae0b3074
gdamss <elbryan@hotmail.fr>
2025-01-28 07:30:17 +0100

    var

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello world !!')
+// declaration
+// var firstName;
+// console.log(firstName);
+
+// initialisation
+// firstName = 1;
+// console.log(firstName);
+
+// var myvar;
+// var myvar2;
+// var myvar3;
+
+// myvar = 123;
+// myvar2 = 55;
+// console.log(myvar, myvar2);
